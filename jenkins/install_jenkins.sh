0cdf2e1508f45b9ac732e6061e9be4522bab28b4
seetharam j <seetharamj704@gmail.com>
2025-08-02 16:47:10 +0530

    Create install_jenkins.sh

---
+++ jenkins/install_jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y openjdk-11-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update -y
+sudo apt install -y jenkins
+sudo systemctl start jenkins
