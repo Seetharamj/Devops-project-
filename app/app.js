a6eeb416a78afbc908699d8f7aa073d21fbcc806
seetharam j <seetharamj704@gmail.com>
2025-08-02 16:50:39 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,11 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.send('Hello from Dockerized App!'))
-app.listen(3000, () => console.log('App running on port 3000'))
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Hello from Dockerized Node.js App!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
